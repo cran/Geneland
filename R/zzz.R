974d3825a9e2c18e088ff1e414c12842f18f42e8
Gilles Guillot <Gilles.Guillot@bio.uio.no>
2008-01-19 00:00:00 +0000

    version 2.0.12

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,20 @@
 .First.lib <- function (lib, pkg) 
 {
   library.dynam("Geneland",pkg, lib)
-
   
   cat('---------------------------------------------------------------------------------',fill=TRUE)
-  cat('-   Geneland-2.0.10 is loaded                                                   -',fill=TRUE)
+  cat('-   Geneland-2.0.12 is loaded                                                   -',fill=TRUE)
   cat('-                                                                               -',fill=TRUE)
-  cat('-       *** Please ***                                                          -',fill=TRUE)
-  cat('-                                                                               -',fill=TRUE)
-  cat('-        Send a short email at guillot@inapg.inra.fr                            -',fill=TRUE)
+  cat('-       * Please *                                                              -',fill=TRUE)
+  cat('-        Send a short email at gilles.guillot@bio.uio.no                        -',fill=TRUE)
   cat('-        notifying that you are using Geneland.                                 -',fill=TRUE)
   cat('-                                                                               -',fill=TRUE)  
   cat('-        See                                                                    -',fill=TRUE)
-  cat('-        www.inapg.inra.fr/ens_rech/mathinfo/personnel/guillot/Geneland.html    -',fill=TRUE)  
-  cat('-        for an introduction to Geneland                                        -',fill=TRUE)
+  cat('-        http://folk.uio.no/gillesg/Geneland.html                               -',fill=TRUE)  
+  cat('-        for a quick introduction.                                             -',fill=TRUE)
+  cat('-        See also                                                               -',fill=TRUE)
+  cat('-        http://folk.uio.no/gillesg/faq.html                                    -',fill=TRUE)
+  cat('-        for frequently asked questions.                                        -',fill=TRUE)  
   cat('---------------------------------------------------------------------------------',fill=TRUE)
 
 }
